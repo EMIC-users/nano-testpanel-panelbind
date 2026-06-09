96eabb1366a8c1f392165e67bd58e67741581df3
EMIC Compiler <emic-compiler@emic.io>
2026-06-09 17:53:02 -0300

    Code generation: 2026-06-09 20:53:02 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: panelbind
    Project: testpanel
    User: nanoloco2

--- userFncFile.c
+++ userFncFile.c
@@ -22,7 +22,7 @@ void onReset()
     counter = 0;
     Graphics_OLED_clear();
     Graphics_OLED_printAt(0, 0, 1, "COUNT:");
-    Graphics_OLED_bindAt(0, 60, 1, &counter, 1, "%4u");
+    Graphics_OLED_bindAt(50, 0, 1, &counter, 1, "%4u");
     setTime1(1000, 'A');
 }
 
